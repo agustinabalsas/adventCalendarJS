6decc9fe51029f3773fe933a52d52e98422bc4d0
agustinabalsas <agustinabalsasm@outlook.com>
2023-08-09 11:59:07 -0300

    ADD: ex. 1

--- script/script.ts
+++ script/script.ts
@@ -1,3 +1,25 @@
-console.log('Hola')
 
-console.log('chau')
+////////// 1
+
+function wrapping(gifts : string[]) {
+
+    let retorno : string[] = [];
+  
+    for (let gift of gifts){
+  
+      let wrapper = "";
+  
+      for (let i = 0; i< gift.length+2; i++){
+        wrapper += "*";
+      }
+  
+      retorno.push(wrapper + "\n*" + gift + "*\n" + wrapper);
+    }
+  
+    return retorno;
+  }
+
+  console.log('Ejercicio 1 --->');
+  console.log(wrapping(["toy", "phone"]));
+
+  
