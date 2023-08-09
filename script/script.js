814331f27c4d0447efda910a1dc044181ef813c3
agustinabalsas <agustinabalsasm@outlook.com>
2023-08-09 12:06:06 -0300

    ADD: ex. 2

--- script/script.js
+++ script/script.js
@@ -13,3 +13,17 @@ function wrapping(gifts) {
 }
 console.log('Ejercicio 1 --->');
 console.log(wrapping(["toy", "phone"]));
+console.log(' ');
+////////// 2
+function countHours(year, holidays) {
+    var accumulated = 0;
+    for (var _i = 0, holidays_1 = holidays; _i < holidays_1.length; _i++) {
+        var holiday = holidays_1[_i];
+        var day = new Date("".concat(holiday, "/").concat(year)).getDay();
+        if (day !== 6 && day !== 0)
+            accumulated += 2;
+    }
+    return accumulated;
+}
+console.log('Ejercicio 2 --->');
+console.log(countHours(2021, ["01/01", "12/02", "25/12"]));
--- script/script.ts
+++ script/script.ts
@@ -1,4 +1,3 @@
-
 ////////// 1
 
 function wrapping(gifts : string[]) {
@@ -21,5 +20,21 @@ function wrapping(gifts : string[]) {
 
   console.log('Ejercicio 1 --->');
   console.log(wrapping(["toy", "phone"]));
+  console.log(' ');
 
+////////// 2
+
+  function countHours(year : number, holidays : string[]) {
+    let accumulated : number = 0;
   
+    for (let holiday of holidays){
+      let day = new Date(`${holiday}/${year}`).getDay();
+  
+      if (day !== 6 && day !== 0) accumulated += 2;
+    }
+  
+    return accumulated;
+  }
+
+  console.log('Ejercicio 2 --->');
+  console.log(countHours(2021, ["01/01", "12/02", "25/12"]));
