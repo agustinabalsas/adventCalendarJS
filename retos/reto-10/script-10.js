8daaf61cacb11ac850b6ad6191293ca16e2678f9
agustina b <agustinabalsasm@outlook.com>
2023-08-29 15:19:55 -0300

    ADD: comienzo reto 10

---
+++ retos/reto-10/script-10.js
@@ -0,0 +1,19 @@
+function checkJump(heights) {
+    // recorro el array
+    // corroborando que sea menor al próximo
+    // cuando el valor siguiente es menor al actual
+    // corroboro que los próximos valores sean siempre menores al próximo
+   
+   
+    for (let i = 0; i < heights.length ; i ++){
+      if (heights[i] > heights[i+1]){
+        false;
+      }else{
+     if (heights[i] > heights[i+1]){
+        for (let j = i; j < heights.length ; j ++){
+    
+        }
+      }
+      }
+    }
+   }
