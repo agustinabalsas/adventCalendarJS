d56406c6fd29df1f5d0552647f1233a49ec7b026
agustina b <abalsas@accesa.com.uy>
2023-08-31 11:40:09 -0300

    ADD:reto 10

--- retos/reto-10/script-10.js
+++ retos/reto-10/script-10.js
@@ -1,19 +1,24 @@
 function checkJump(heights) {
-    // recorro el array
-    // corroborando que sea menor al próximo
-    // cuando el valor siguiente es menor al actual
-    // corroboro que los próximos valores sean siempre menores al próximo
-   
-   
-    for (let i = 0; i < heights.length ; i ++){
-      if (heights[i] > heights[i+1]){
-        false;
-      }else{
-     if (heights[i] > heights[i+1]){
-        for (let j = i; j < heights.length ; j ++){
-    
-        }
-      }
-      }
+  let posAlta = 0;
+
+  if (heights.length <= 1 || (heights.indexOf(Math.max(...heights)) == 0)){
+    return false
+  }
+
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] > heights[i + 1]) {
+      posAlta = i;
+      break;
     }
-   }
+  }
+
+  for (let j = posAlta; j < heights.length; j++) {
+    if (heights[j] <= heights[j + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(checkJump([1]) )
